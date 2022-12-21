a146f1a93ef55f8bc9839902d99bbf58d8758004
Rob-McCormack <rob.a.mccormack@gmail.com>
2022-12-20 20:47:32 -0500

    various fixes, css etc.

---
+++ js/terminal_resume_copy.js
@@ -0,0 +1,100 @@
+window.console = window.console || function (t) { };
+
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+
+}
+
+var captionLength = 0;
+
+const d = new Date();
+let currentDate = d.toDateString();
+// currentDate = d.toString()
+
+// var dateFormat = require('dateformat');
+// var now = new Date();
+// currentDate = dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
+
+// currentDate = new Date().toDateString()
+
+
+var caption1 = `  I am an experienced <mark>Python programmer</mark> 
+ with a strong background in computer science
+ and software development.<br>
+ ~ Rob` + ' . . . . . . . . ' + currentDate;
+
+
+$(document).ready(function () {
+    setInterval('cursorAnimation()', 600);
+    captionEl = $('#caption');
+
+    testTypingEffect()
+
+    $('#test-typing').click(function () {
+        testTypingEffect();
+    });
+
+    $('#test-erasing').click(function () {
+        testErasingEffect();
+    });
+});
+
+function testTypingEffect() {
+    caption = caption1;
+    type();
+}
+
+function type() {
+    captionEl.html(caption.substr(0, captionLength++));
+    if (captionLength < caption.length + 1) {
+        setTimeout('type()', 50);
+    } else {
+        captionLength = 0;
+        caption = '';
+        allDone()
+
+    }
+}
+
+function allDone() {
+    // < !--document.getElementById('test-erasing').display = block-- >
+    // alert('type all done Rob')
+
+    const element1 = document.getElementById("fa-3x");
+    element1.style.display = "block";
+
+
+}
+
+function testErasingEffect() {
+    caption = captionEl.html();
+    captionLength = caption.length;
+    if (captionLength > 0) {
+        erase();
+    } else {
+        $('#caption').html("You didn't write anything to erase, but that's ok!");
+        setTimeout('testErasingEffect()', 1000);
+    }
+}
+
+function erase() {
+    captionEl.html(caption.substr(0, captionLength--));
+    if (captionLength >= 0) {
+        setTimeout('erase()', 50);
+    } else {
+        captionLength = 0;
+        caption = '';
+    }
+}
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    },
+        'fast', 'swing').animate({
+            opacity: 1
+        },
+            'fast', 'swing');
+}
+
